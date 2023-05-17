497e0bf4e2602dfde76c0e76a4d73ff876f528aa
unknown <mvleite0908@gmail.com>
2023-05-16 21:35:36 -0300

    feat: register page

---
+++ src/app/pages/financial/financial.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FinancialComponent } from "./financial.component";
+import { SummaryComponent } from "./components/summary/summary.component";
+import { RegisterComponent } from "./components/register/register.component";
+import { RouterModule, Routes } from "@angular/router";
+import { ExpensesTableComponent } from "./components/expenses-table/expenses-table.component";
+import { NbCardModule } from "@nebular/theme";
+import { Ng2SmartTableModule } from "ng2-smart-table";
+
+const routes: Routes = [
+  {
+    path: "summary",
+    component: SummaryComponent,
+  },
+  {
+    path: "register",
+    component: RegisterComponent,
+  },
+];
+
+@NgModule({
+  declarations: [
+    FinancialComponent,
+    SummaryComponent,
+    RegisterComponent,
+    ExpensesTableComponent,
+  ],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    NbCardModule,
+    Ng2SmartTableModule,
+  ],
+  exports: [RouterModule],
+})
+export class FinancialModule {}
